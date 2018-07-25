4425609c4d72918c3101440b225c85a47840f85f
Kelsey Fecho <kfecho30@gmail.com>
2018-07-25 13:47:20 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component{
+
+  fun = (event) => {
+    this.props.onReceiveCoordinates([event.screenX, event.screenY])
+  }
+
+  render(){
+    return(
+      <button onClick={this.fun}>Get Coordinates</button>
+    )
+  }
+}
